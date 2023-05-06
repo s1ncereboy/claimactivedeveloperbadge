40cd1bac44caef90b6d513a56f3226887d8d2462
s1ncereboy <smirnov8653@gmail.com>
2023-05-07 00:30:56 +0300

    bot

---
+++ index.js
@@ -0,0 +1,47 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessageTyping,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.DirectMessageReactions,
+        GatewayIntentBits.DirectMessageTyping,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+
+client.once(Events.ClientReady, () => {
+    console.log(`Bot  ${client.user.tag} runned!`);
+
+    try {
+        client.application.commands.create({
+            name: 'claim',
+            description: 'just run this command to check if bot is working.',
+            options: []
+        });
+        console.log('Command created successfully.');
+    } catch (error) {
+        console.log('Error.');
+    }
+
+});
+
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    try {
+        if (interaction.commandName == 'claim') {
+            await interaction.reply({content: `Command run successfully. Now need to wait some time. Hope it works, Magog.`, ephemeral: true});
+            return;
+        }
+    } catch (error) {
+        await interaction.reply({content: `Something went wrong while executing command`, ephemeral: true});
+    }
+});
+
+client.login(process.env.TOKEN);
